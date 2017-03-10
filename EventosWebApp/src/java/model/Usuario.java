6a7376b3063b1fa8f076e86a2124ddf4e454ae98
Elcio <elciotaira@hotmail.com>
2017-03-10 20:26:03 -0300

    Criei a Classe Usuario com seus atributos e metodos

---
+++ EventosWebApp/src/java/model/Usuario.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ * <p>
+ * Essa Classe faz a modelagem do usuario, que é a entidade que acessa o sistema
+ * EventosWebApp e usufrui das suas funcionalides:
+ * </p>
+ * <ul>
+ * <li>fazer login</li>
+ * <li>divulgar evento</li>
+ * <li>curtir evento</li>
+ * <li>cancelar curtir evento</li>
+ * <li>nao curtir evento</li>
+ * <li>cancelar não curtir evento</li>
+ * </ul>
+ *
+ * @author Elcio Cestari Taira
+ * @version 1.0
+ * @since 10/mar/2017
+ */
+public class Usuario {
+
+    private String login;
+    private String nome;
+    private int idade;
+    private int id_usuario;
+    private String senha;
+
+    public Usuario(String login, String nome, int idade, int id_usuario, String senha) {
+        this.setId_usuario(id_usuario);
+        this.setIdade(idade);
+        this.setLogin(login);
+        this.setNome(nome);
+        this.setSenha(senha);
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    /**
+     * Metodo privado pois deve ser utilizado somente pelos objetos Usuario e em
+     * seu construtor.
+     *
+     * @param id_usuario
+     *
+     */
+    private void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * Configura o nome do usuario e caso o parametro passado seja uma String
+     * vazia ele lançara uma IllegalArgumentException
+     *
+     * @param nome
+     * @throws IllegalArgumentException
+     */
+    public void setNome(String nome) throws IllegalArgumentException {
+        if (nome.equals("")) {
+            throw new IllegalArgumentException("O nome do usuário não pode ser vazio");
+        } else {
+            this.nome = nome;
+        }
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * Configura a idade de um usuário aceitando apenas numero inteiro e maior
+     * que 0
+     *
+     * @param idade
+     * @throws IllegalArgumentException
+     */
+    public void setIdade(int idade) throws IllegalArgumentException {
+        if (idade < 1) {
+            throw new IllegalArgumentException("Idade informada é invalida! Por favor, informe um numero! Exemplo: 20");
+        } else {
+            this.idade = idade;
+        }
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * Configura a senha do usuário e caso seja preenchido o campo sem a senha
+     * sera lançada a exceção IllegalArgumentException
+     *
+     * @param senha
+     * @throws IllegalArgumentException
+     */
+    public void setSenha(String senha) throws IllegalArgumentException {
+        if (senha.equals("")) {
+            throw new IllegalArgumentException("Uma senha é obrigátoria!");
+        } else {
+            this.senha = senha;
+        }
+    }
+
+    /**
+     * retorna uma String contendo o nome o login e a idade de um usuario no
+     * seguinte formato:
+     * <strong>Nome: Fulano da Silva Idade: 30 Login:
+     * meulogin@email.com</strong>
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "Nome: " + this.getNome()
+                + "Idade: " + this.getIdade()
+                + "Login: " + this.getLogin();
+    }
+
+}
--- EventosWebApp/web/Home.jsp
+++ EventosWebApp/web/Home.jsp
@@ -1,36 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>${tituloDaPagina}</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
+    <head>
+        <title>${tituloDaPagina}</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
 
-<header class="jumbotron text-center">
-  <h1>My First Bootstrap Page</h1>
-  <p>Resize this responsive page to see the effect!</p> 
-</header>
-  
-<div class="container">
-  <div class="row">
-    <img src="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjl2LnC7srSAhVFG5AKHUn6BEIQjRwIBQ&url=http%3A%2F%2Fwww.fatosdesconhecidos.com.br%2F8-principais-diferencas-entre-festas-gays-e-festas-heterossexuais%2F&psig=AFQjCNE0sS53BjBEYdTPLelMjUOilcL__Q&ust=1489198408712906" class="col-sm-4">
-    </img>
-    <div class="col-sm-8">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>        
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-  </div>
-</div>
+        <header class="jumbotron text-center">
+            <h1>Eventos Campo Grande</h1>
+            <p>Encontre e divulgue os seus eventos preferidos!</p> 
+        </header>
 
-</body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcStfDAuvu8lCkbu5ugjEKhsT00G01fnZLhSAbhNFbiza8MIFZfUjYA8n4A">
+                    </img>
+                </div>
+                <div class="col-sm-8">
+                    <h3>Titulo da Festa</h3>
+                    <p>Aqui vai uma breve descrição do que conterá na festa</p>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                </div>
+            </div>
+        </div>
+
+    </body>
 </html>
